6da32695be6a1e65b9638373c068698f17e91162
root <root@nova-20.lyon.grid5000.fr>
2021-03-12 11:26:22 +0100

    bug fix

--- tools/bots-benchs.sh
+++ tools/bots-benchs.sh
@@ -12,40 +12,41 @@ output_dir=all-output
 mkdir $output_dir
 
 echo -e "Running debian benchs...\n"
-./run.sh ./ . $bots_location/debian_bin/fib.*.omp-tasks 100
-./run.sh ./ . $bots_location/debian_bin/health.*.omp-tasks "-f $bots_location/inputs/health/medium.input" 100
-./run.sh ./ . $bots_location/debian_bin/sparselu.*.for-omp-tasks 100
-./run.sh ./ . $bots_location/debian_bin/strassen.*.omp-tasks 100
-./run.sh ./ . $bots_location/debian_bin/fib.*.omp-tasks 100
+./run.sh ./ . $bots_location/debian_bin/fib.*.omp-tasks "" 50
+./run.sh ./ . $bots_location/debian_bin/health.*.omp-tasks "-f $bots_location/inputs/health/medium.input" 50
+./run.sh ./ . $bots_location/debian_bin/sparselu.*.for-omp-tasks "" 50
+./run.sh ./ . $bots_location/debian_bin/strassen.*.omp-tasks "" 50
+./run.sh ./ . $bots_location/debian_bin/fib.*.omp-tasks "" 50
 
 mv outputs $output_dir/debian-omp-outputs
 
 echo -e "Running hermitcore benchs...\n"
-./run.sh hermitcore /opt/hermit $bots_location/hermitcore_bin/fib.*.omp-tasks 100
-./run.sh hermitcore /opt/hermit $bots_location/hermitcore_bin/health.*.omp-tasks "-f $bots_location/inputs/health/medium.input" 100
-./run.sh hermitcore /opt/hermit $bots_location/hermitcore_bin/sparselu.*.for-omp-tasks 100
-./run.sh hermitcore /opt/hermit $bots_location/hermitcore_bin/strassen.*.omp-tasks 100
-./run.sh hermitcore /opt/hermit $bots_location/hermitcore_bin/fib.*.omp-tasks 100
+./run.sh hermitcore /opt/hermit $bots_location/hermitcore_bin/fib.*.omp-tasks "" 50
+./run.sh hermitcore /opt/hermit $bots_location/hermitcore_bin/health.*.omp-tasks "-f $bots_location/inputs/health/medium.input" 50
+./run.sh hermitcore /opt/hermit $bots_location/hermitcore_bin/sparselu.*.for-omp-tasks "" 50
+./run.sh hermitcore /opt/hermit $bots_location/hermitcore_bin/strassen.*.omp-tasks "" 50
+./run.sh hermitcore /opt/hermit $bots_location/hermitcore_bin/fib.*.omp-tasks "" 50
 
 mv outputs $output_dir/hermitcore-omp-outputs
 
 echo -e "Running hermitux benchs...\n"
-./run.sh hermitux $HOME/hermitux $bots_location/hermitux_bin/fib.*.omp-tasks 100
-./run.sh hermitux $HOME/hermitux $bots_location/hermitux_bin/health.*.omp-tasks "-f $bots_location/inputs/health/medium.input" 100
-./run.sh hermitux $HOME/hermitux $bots_location/hermitux_bin/sparselu.*.for-omp-tasks 100
-./run.sh hermitux $HOME/hermitux $bots_location/hermitux_bin/strassen.*.omp-tasks 100
-./run.sh hermitux $HOME/hermitux $bots_location/hermitux_bin/fib.*.omp-tasks 100
+./run.sh hermitux $HOME/hermitux $bots_location/hermitux_bin/fib.*.omp-tasks "" 50
+./run.sh hermitux $HOME/hermitux $bots_location/hermitux_bin/health.*.omp-tasks "-f $bots_location/inputs/health/medium.input" 50
+./run.sh hermitux $HOME/hermitux $bots_location/hermitux_bin/sparselu.*.for-omp-tasks "" 50
+./run.sh hermitux $HOME/hermitux $bots_location/hermitux_bin/strassen.*.omp-tasks "" 50
+./run.sh hermitux $HOME/hermitux $bots_location/hermitux_bin/fib.*.omp-tasks "" 50
 
 mv outputs $output_dir/hermitux-omp-outputs
 
 for benchs in $output_dir/*; do
-    cd $bench
+    cd $benchs
     touch times.csv
     for file in *; do
         echo "$file;" >> times.csv
         cat $file | grep "Time Program" | awk '{print $4 ";"}' >> times.csv;
         echo >> times.csv
     done
+    cd ../..
 done
 
 echo "Benchs finished !"
