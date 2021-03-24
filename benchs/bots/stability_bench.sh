7eebd1dabce43e6d4eb704f3ebfed234ccf2581d
p-jacquot <pierre.jacquot@insa-lyon.fr>
2021-03-24 15:57:55 +0100

    Resume experience when csv files do not exist.

--- benchs/bots/stability_bench.sh
+++ benchs/bots/stability_bench.sh
@@ -15,16 +15,24 @@ if [ ! -e $output_folder ]; then
 fi
 
 for cpu in $cpus_list; do
+
     echo -e "Doing stability tests for $cpu cores.\n===="
     export HERMIT_CPUS=$cpu
     export OMP_NUM_THREADS=$cpu
-    csvfilename=$cpu-cores-$command_file_name.csv
-    logfilename=$cpu-cores-$command_file_name.log
-    cat $command_file | while read timeout_args; do
-        ./stability.sh $n_try $timeout_args
-    done
-    mv exec.csv $output_folder/$csvfilename
-    mv exec.log $output_folder/$logfilename
+    csvfile=$output_folder/$cpu-cores.csv
+    logfile=$output_folder/$cpu-cores.log
+    if [ ! -e $csvfile ] | [ ! -e $logfile ]; then
+        cat $command_file | while read timeout_args; do
+            ./stability.sh $n_try $timeout_args
+        done
+        mv exec.csv $csvfile
+        mv exec.log $logfile
+    else
+        echo -e "$csvfile already exists."
+        echo -e "Skipping stability tests for $cpu cores."
+    fi
+
+    echo -e "\n"
 done
 
 rm stability.sh
