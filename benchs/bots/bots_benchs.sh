409e4174a52f799f9a2e5a95a8a3bb26fe96d4de
p-jacquot <pierre.jacquot@insa-lyon.fr>
2021-03-24 15:22:23 +0100

    enhanced bots benchs.

--- benchs/bots/bots_benchs.sh
+++ benchs/bots/bots_benchs.sh
@@ -1,8 +1,19 @@
 #! /bin/bash
 
 command_files_list=$1
-n=$2
-cpus_list=$3
+destination_folder=$2
+n=$3
+cpus_list=$4
+
+current_dir=$(pwd)
+
+if (( $# < 4 )); then
+    echo -e "Error : missing arguments."
+    echo -e "Command syntax : $0 <command_files_list> <destination_folder> <number of measures> <cores_list>"
+    echo -e "Aborting script."
+    exit 1
+fi 
+
 
 time_file=times.csv
 
@@ -11,20 +22,33 @@ ln -s ../../tools/timeout_run.sh timeout_run.sh
 
 for command_file in $command_files_list; do
     command_file_name=$(basename $command_file)
-    ./benchs.sh $command_file $n "$cpus_list"
+    echo -e "Executing commands from $command_file :\n\n"
+    ./benchs.sh $command_file $destination_folder $n "$cpus_list"
 
-    cd results-$command_file_name
-    for folder in *; do
-        cd ./$folder
-        touch $time_file
-        for logfile in *.log; do
-            echo "$logfile;" >> $time_file
-            cat $logfile | grep "Time Program" | awk '{print $4 ";"}' >> $time_file
-            echo ";" >> $time_file
+    results_folder=$destination_folder/results-$command_file_name
+    if [ ! -e $results_folder ]; then
+        echo -e "Error : expected to find $results_folder, but the directory does not exist."
+        echo -e "Skipping formatting of thoses results."
+    else
+        cd $results_folder
+        for folder in *; do
+            if [ ! -e $folder/$time_file ]; then
+                cd ./$folder
+                touch $time_file
+                for logfile in *.log; do
+                    echo "$logfile;" >> $time_file
+                    cat $logfile | grep "Time Program" | awk '{print $4 ";"}' >> $time_file
+                    echo ";" >> $time_file
+                done
+                cd ..
+            else
+                echo -e "File $folder/$time_file already exists."
+                echo -e "Skipping formatting."
+            fi
         done
-        cd ..
-    done
-    cd ..
+        cd $current_dir
+    fi
+    echo -e "\n"
 done
 
 rm benchs.sh
