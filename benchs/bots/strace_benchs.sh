91e0454ca80d132cdd2423bb1cd4ed21e215c055
p-jacquot <pierre.jacquot@insa-lyon.fr>
2021-03-25 11:45:05 +0100

    Perform strace -c on executables.

---
+++ benchs/bots/strace_benchs.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+command_file=$1
+destination_folder=$2
+ncpus=$3
+
+if (( $# < 3 )); then
+    echo -e "Error : missing arguments."
+    echo -e "Command syntax : $0 <command_file> <destination_folder> <number of cores>"
+    echo -e "Aborting script."
+    exit 1
+fi
+
+if [ ! -e $command_file ]; then
+    echo -e "Error : $command_file file does not exists."
+    echo -e "Aborting script."
+    exit 1
+else if [ ! -d $destination_folder ]; then
+    echo -e "Error : $destination_folder does not exists, or is not a directory."
+    echo -e "Aborting script."
+    exit 1
+fi
+fi
+
+ln -s ../../tools/strace_run.sh strace_run.sh
+
+cat $command_file | while read strace_args; do
+        echo "Issuing command ./strace_run.sh $destination_folder $strace_args"
+        return_code=1
+        while [ $return_code != "0" ]; do
+            ./strace_run.sh $destination_folder $strace_args
+            return_code=$?
+        done
+        echo 
+done
+
+rm strace_run.sh
+
--- tools/strace_run.sh
+++ tools/strace_run.sh
@@ -1,48 +1,75 @@
 #! /bin/bash
 
-unikernel=$1
-unikernel_dir=$2
+destination_folder=$1
 
-prog=$3
+unikernel=$2
+unikernel_dir=$3
+
+time_limit=$4
+
+prog=$5
 args=
 
-while [ -n "$4" ]; do
-    args="$args $4"
+while [ -n "$6" ]; do
+    args="$args $6"
     shift
 done
 
 prog_name=$(basename $prog)
-strace="strace -c -o $prog_name.trace"
+output_file=$destination_folder/$prog_name.trace
+timeout="timeout -s SIGKILL --foreground $time_limit"
+strace="strace -c -o $output_file"
+
+if [ -e $output_file ]; then
+    echo -e "$output_file already exists."
+    echo -e "Skipping this execution."
+    exit 0
+fi
+
+return_code=0
 
 case $unikernel in
     "hermitcore")
-        $strace $unikernel_dir/bin/proxy $prog $args
+        $timeout $strace $unikernel_dir/bin/proxy $prog $args
+        return_code=$?
         ;;
 
     "hermitux")
         HERMIT_ISLE=uhyve HERMIT_TUX=1 \
             HERMIT_SECCOMP=0 HERMIT_DEBUG=0 HERMIT_MEM=4G \
-            $strace \
+            $timeout $strace \
             $unikernel_dir/hermitux-kernel/prefix/bin/proxy \
             $unikernel_dir/hermitux-kernel/prefix/x86_64-hermit/extra/tests/hermitux \
             $prog $args
+        return_code=$?
         ;;
 
     "rumprun")
-        $strace \
+        $timeout $strace \
             $unikernel_dir/rumprun/rumprun/bin/rumprun kvm -i -g '-nographic -vga none' $prog $args
+        return_code=$?
         ;;
 
     "./")
-        $strace ./$prog $args
+        $timeout $strace ./$prog $args
+        return_code=$?
         ;;
 
     *)
         echo "Unknown unikernel : $unikernel."
         echo "You can avoid using an unikernel by using the following command :"
         echo "$0 ./ . <program> [args]"
+        exit 1
         ;;
 esac
+
+if [ $return_code != "0" ];then
+    error_output=$output_file.error
+    echo -e "An error occured while running the executable."
+    echo -e "Traces will be moved to an error file : $error_output"
+    cat $output_file >> $error_output
+    rm $output_file
+fi
 
 exit $?
 
