43677485be97a74279b9f806a2c9606f15e05891
p-jacquot <pierre.jacquot@insa-lyon.fr>
2021-04-09 15:04:31 +0200

    Removed unused scripts.

--- benchs/bots/launch_stability.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-cpu_list="2 4 8 16"
-
-./stability_bench.sh stability-commands/serial-commands 1 50 120 hermitux /root/hermitux bots/bin-hermitux
-./stability_bench.sh stability-commands/serial-commands 1 50 120 hermitcore /opt/hermit bots/bin-hermitcore
-
-./stability_bench.sh stability-commands/omp-commands "$cpu" 50 50 hermitux /root/hermitux bots/bin-hermitux
-./stability_bench.sh stability-commands/omp-commands "$cpu" 50 120 hermitcore /opt/hermit bots/bin-hermitcore
