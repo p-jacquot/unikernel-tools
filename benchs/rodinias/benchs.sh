0a99bc8d4f5e3ae3e6914114928657f14784376f
p-jacquot <pierre.jacquot@insa-lyon.fr>
2021-04-29 10:12:01 +0200

    Add rodinias benchs.

---
+++ benchs/rodinias/benchs.sh
@@ -0,0 +1,77 @@
+#! /bin/bash
+
+debian_exec(){
+    output_file=$1
+    prog=$2
+    shift
+    shift
+    args=$@
+    echo "./debian-bin/$prog $args >> $output_file"
+}
+
+hermitux_exec(){
+    output_file=$1
+    prog=$2
+    shift
+    shift
+    args=$@
+    export HERMIT_ISLE=uhyve
+    export HERMIT_TUX=1
+    export HERMIT_CPUS=$n_cpus
+    export HERMIT_MEM=4G
+    echo "$hermitux_dir/hermitux-kernel/prefix/bin/proxy \
+        $hermitux_dir/hermitux-kernel/prefix/x86_64-hermit/extra/test/hermitux \
+        hermitux-bin/$prog $args >> $output_file"
+}
+
+repeat(){
+    n_times=$1
+    n_cpus=$2
+    prog_name=$3
+    shift
+    shift
+    shift
+    args=$@
+
+    for ((i = 0; i < $n_times; i++)); do
+        debian_exec $prog_name.log $prog_name $args
+    done
+    mv $prog_name.log $debian_output_dir/$cpu_folder/
+
+    for ((i = 0; i < $n_times; i++)); do
+        hermitux_exec $prog_name.log $prog_name $args
+    done
+    mv $prog_name.log $hermitux_output_dir/$cpu_folder/
+}
+
+hermitux_dir=/root/hermitux
+
+if [ $# -lt 3 ]; then
+    echo -e "Error : not enough arguments provided."
+    echo -e "commands syntax : $0 <output_dir> <n_times> <cpus_list>"
+    exit 1
+fi
+
+
+output_dir=$1
+n=$2
+cpus_list=$3
+
+debian_output_dir=$output_dir/debian
+hermitux_output_dir=$output_dir/hermitux
+
+mkdir $debian_output_dir
+mkdir $hermitux_output_dir
+
+for cpu in $cpus_list; do
+    
+    cpu_folder=$cpu-cores
+    mkdir $debian_output_dir/$cpu_folder
+    mkdir $hermitux_output_dir/$cpu_folder
+
+    repeat $n $cpu bfs $cpu inputs/bfs/graph1MW_6.txt
+    repeat $n $cpu kmeans_openmp -n $cpu -i inputs/kmeans/kdd_cup.txt
+    repeat $n $cpu lavaMD -cores $cpu -boxes1d 10
+    repeat $n $cpu lud_omp -n $cpu -i input/lud/512.dat
+
+done
